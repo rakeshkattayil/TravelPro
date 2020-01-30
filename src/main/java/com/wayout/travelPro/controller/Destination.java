355962ad65fb1c55ae8321c4d8e51a108601a58f
unknown <rakeshkattayil@gmail.com>
2020-01-30 12:59:41 +0530

    Created sample webservice for greeting

--- src/main/java/com/wayout/travelPro/controller/Destination.java
+++ src/main/java/com/wayout/travelPro/controller/Destination.java
@@ -1,4 +1,15 @@
 package com.wayout.travelPro.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/destination")
 public class Destination {
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "Welcome to TravelPro!!!!";
+    }
 }
